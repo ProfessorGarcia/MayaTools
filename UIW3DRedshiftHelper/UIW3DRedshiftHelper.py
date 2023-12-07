2fa2db51b1610f8666a209a946c381a52d1cfff4
Carlos Garcia <121709223+ProfessorGarcia@users.noreply.github.com>
2023-12-07 15:51:21 -0600

    Update UIW3DRedshiftHelper.py
    JT Mastermind Fix!

--- UIW3DRedshiftHelper/UIW3DRedshiftHelper.py
+++ UIW3DRedshiftHelper/UIW3DRedshiftHelper.py
@@ -1,6 +1,42 @@
 from PySide2 import QtWidgets, QtGui, QtCore
 import maya.cmds as cmds
 
+from PySide2 import QtWidgets, QtGui, QtCore
+
+class CollapsibleSection(QtWidgets.QWidget):
+    def __init__(self, title="", parent=None):
+        super(CollapsibleSection, self).__init__(parent)
+
+        self.toggle_button = QtWidgets.QToolButton(text=title, checkable=True, checked=False)
+        self.toggle_button.setStyleSheet("QToolButton { border: none; }")
+        self.toggle_button.setToolButtonStyle(QtCore.Qt.ToolButtonTextBesideIcon)
+        self.toggle_button.setArrowType(QtCore.Qt.RightArrow)
+        self.toggle_button.toggled.connect(self.toggle)
+
+        self.content_area = QtWidgets.QWidget()
+        self.content_layout = QtWidgets.QVBoxLayout()
+        self.content_area.setLayout(self.content_layout)
+
+        main_layout = QtWidgets.QVBoxLayout(self)
+        main_layout.addWidget(self.toggle_button)
+        main_layout.addWidget(self.content_area)
+        main_layout.addStretch()
+        main_layout.setSpacing(0)
+        main_layout.setContentsMargins(0, 0, 0, 0)
+
+        self.setLayout(main_layout)
+
+    def toggle(self):
+        if self.toggle_button.isChecked():
+            self.content_area.setVisible(True)
+            self.toggle_button.setArrowType(QtCore.Qt.DownArrow)
+            self.setMaximumHeight(self.sizeHint().height())
+        else:
+            self.content_area.setVisible(False)
+            self.toggle_button.setArrowType(QtCore.Qt.RightArrow)
+            self.setMaximumHeight(self.toggle_button.sizeHint().height())
+
+        
 class UIW3DRedshiftHelper(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super(UIW3DRedshiftHelper, self).__init__(parent)
@@ -17,13 +53,19 @@ class UIW3DRedshiftHelper(QtWidgets.QWidget):
         # Create Render Settings Group
         custom_settings_group = QtWidgets.QGroupBox("Render Settings")
         custom_settings_layout = QtWidgets.QVBoxLayout(custom_settings_group)
+        
 
         # Create sub-group for Custom Settings fields
+ 
         custom_settings_fields_group = QtWidgets.QGroupBox("Custom Settings Fields")
         custom_settings_fields_layout = QtWidgets.QFormLayout()
 
         self.width_input = QtWidgets.QLineEdit("1920")
         self.height_input = QtWidgets.QLineEdit("1080")
+        
+        trace_depth_settings_fields_group = QtWidgets.QGroupBox("Trace Depth Settings")
+        trace_depth_settings_fields_layout = QtWidgets.QFormLayout()
+        
         self.combined_depth_input = QtWidgets.QLineEdit("6")
         self.num_gi_bounces_input = QtWidgets.QLineEdit("4")
         self.reflection_depth_input = QtWidgets.QLineEdit("2")
@@ -33,16 +75,26 @@ class UIW3DRedshiftHelper(QtWidgets.QWidget):
 
         custom_settings_fields_layout.addRow("Width:", self.width_input)
         custom_settings_fields_layout.addRow("Height:", self.height_input)
-        custom_settings_fields_layout.addRow("Combined:", self.combined_depth_input)
-        custom_settings_fields_layout.addRow("Global Illumination:", self.num_gi_bounces_input)
-        custom_settings_fields_layout.addRow("Reflection:", self.reflection_depth_input)
-        custom_settings_fields_layout.addRow("Refraction:", self.refraction_depth_input)
-        custom_settings_fields_layout.addRow("Volume:", self.volume_max_trace_depth_input)
-        custom_settings_fields_layout.addRow("Transparency:", self.transparency_max_trace_depth_input)
+        trace_depth_settings_fields_layout.addRow("Combined:", self.combined_depth_input)
+        trace_depth_settings_fields_layout.addRow("Global Illumination:", self.num_gi_bounces_input)
+        trace_depth_settings_fields_layout.addRow("Reflection:", self.reflection_depth_input)
+        trace_depth_settings_fields_layout.addRow("Refraction:", self.refraction_depth_input)
+        trace_depth_settings_fields_layout.addRow("Volume:", self.volume_max_trace_depth_input)
+        trace_depth_settings_fields_layout.addRow("Transparency:", self.transparency_max_trace_depth_input)
 
         custom_settings_fields_group.setLayout(custom_settings_fields_layout)
         custom_settings_layout.addWidget(custom_settings_fields_group)
+        
 
+        
+        testLayout = CollapsibleSection(title="Settings")
+        self.layout.addWidget(testLayout)
+        testLayout.content_layout.addWidget(custom_settings_fields_group)
+        
+        trace_depth_settings_fields_group.setLayout(trace_depth_settings_fields_layout)
+        custom_settings_layout.addWidget(trace_depth_settings_fields_group)
+        testLayout.content_layout.addWidget(trace_depth_settings_fields_group)
+        
         # Apply Custom Settings Button
         self.apply_custom_settings_button = QtWidgets.QPushButton("Apply Custom Settings")
         self.apply_custom_settings_button.clicked.connect(self.apply_custom_settings)
@@ -53,9 +105,11 @@ class UIW3DRedshiftHelper(QtWidgets.QWidget):
         self.quick_render_settings_button.clicked.connect(self.apply_quick_settings)
         self.quick_render_settings_button.setStyleSheet("QPushButton { background-color: #c9103b; border: 10px solid #c9103b; color: white; } QPushButton:pressed { background-color: #8e0922; border: 10px solid #8e0922; color: white; }")
 
-        custom_settings_layout.addWidget(self.apply_custom_settings_button)
+        #custom_settings_layout.addWidget(self.apply_custom_settings_button)
         custom_settings_layout.addWidget(self.quick_render_settings_button)
-
+        
+        
+        testLayout.content_layout.addWidget(self.apply_custom_settings_button)
         self.layout.addWidget(custom_settings_group)
 
         # Convert Lambert to Redshift Group
@@ -69,6 +123,36 @@ class UIW3DRedshiftHelper(QtWidgets.QWidget):
 
         convert_to_redshift_layout.addWidget(self.convert_lambert_button)
         self.layout.addWidget(convert_to_redshift_group)
+    
+    def create_settings_fields(self, layout):
+            # Create layout for settings
+            settings_layout = QtWidgets.QFormLayout()
+    
+            # Resolution
+            self.width_input = QtWidgets.QLineEdit("1920")
+            self.height_input = QtWidgets.QLineEdit("1080")
+            settings_layout.addRow("Resolution Width:", self.width_input)
+            settings_layout.addRow("Resolution Height:", self.height_input)
+    
+            # Reflection Trace Depth
+            self.reflection_depth_input = QtWidgets.QLineEdit("2")
+            settings_layout.addRow("Reflection Trace Depth:", self.reflection_depth_input)
+    
+            # Refraction Trace Depth
+            self.refraction_depth_input = QtWidgets.QLineEdit("2")
+            settings_layout.addRow("Refraction Trace Depth:", self.refraction_depth_input)
+    
+            # Combined Trace Depth
+            self.combined_depth_input = QtWidgets.QLineEdit("6")
+            settings_layout.addRow("Combined Trace Depth:", self.combined_depth_input)
+    
+            # Apply Custom Settings Button
+            self.apply_custom_settings_button = QtWidgets.QPushButton("Apply Custom Settings")
+            self.apply_custom_settings_button.clicked.connect(self.apply_custom_settings)
+            settings_layout.addRow(self.apply_custom_settings_button)
+    
+            layout.addLayout(settings_layout)
+
     
     def apply_quick_settings(self):
         # Apply predefined quick settings
